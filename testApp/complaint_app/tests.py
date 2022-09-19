3502fd1d479b02341ecd8fe9b4d57cb28a6f4ae1
Mandy Yu <yumandee@yahoo.com>
2022-09-18 20:34:28 -0400

    all complaints api done and to be tested

--- testApp/complaint_app/tests.py
+++ testApp/complaint_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from complaint_app.models import User
 
-# Create your tests here.
+client = APIClient()
+
+user = User.objects.get(username='mandeenycc')
+
+client.force_authenticate(user=user)
+
+response = client.get('http://127:0.0.1:8000/api/complaints/')
+response.render() # Render to access response.content
+print(response.content)
+client.credentials()
+client.logout()
+
+assert response.status_code == 200
--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -5,23 +5,47 @@ from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
 
+from datetime import date
+
 class ComplaintViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
-  serializer_class = ComplaintSerializer
+  # serializer_class = ComplaintSerializer
+  queryset = Complaint.objects.all()
+  
   def list(self, request):
     # Get all complaints from the user's district
-    return Response()
+    serializer = ComplaintSerializer(self.queryset, many=True)
+    return Response(serializer.data)
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
+  
+  # Open complaints: Current date is gte opendate and lte closedate
+  current_date = date.today()
+  queryset = Complaint.objects.filter(
+    opendate__lte=current_date
+  ).filter(
+    closedate__gte=current_date
+  )
+  
   def list(self, request):
     # Get only the open complaints from the user's district
-    return Response()
+    serializer = ComplaintSerializer(self.queryset, many=True)
+    return Response(serializer.data)
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get'] 
   def list(self, request):
     # Get only complaints that are close from the user's district
+    # Closed complaints: closedate is less than current date
+    # By default, closedate should be greater than opendate
+    # Request should pass user's district?
+    
+    # closed_complaints = Complaint.objects.get(
+    #   district__exact=0
+    # ).filter(
+    #   closedate__lt=date.today()
+    # )
     return Response()
     
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
